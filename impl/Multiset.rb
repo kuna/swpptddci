3dc947473db7a86861e22913366875cbe6e4b4d7
kuna <kgdgwn98@gmail.com>
2015-05-04 20:32:13 +0900

    modified gitignore, added test

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,17 @@
+class Multiset
+  def initialize(n)
+    @arr = [n]
+  end
 
+  def contains(n)
+    return @arr.include? n
+  end
+
+  def add(n)
+    @arr.push(n)
+  end
+
+  def remove(n)
+    @arr.delete_at(@arr.find_index(n))
+  end
+end
